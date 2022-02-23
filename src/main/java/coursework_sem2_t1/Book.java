8ea01be80a7f9619f39dd070b41f608a50c9b268
wishyoudie <66038379+wishyoudie@users.noreply.github.com>
2022-02-23 20:33:03 +0300

    Initial commit

---
+++ src/main/java/coursework_sem2_t1/Book.java
@@ -0,0 +1,242 @@
+/* FILE NAME: Book.java
+ * PROGRAMMER: Ilyin Vladimir, group 3530901/10005, GitHub: @wishyoudie
+ * DATE: 02/23/2022
+ * PURPOSE: Implementation of Address book class.
+ *          Realisation module.
+ */
+
+/* Package */
+package coursework_sem2_t1;
+
+/* Imports */
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+
+/*
+ * Address book implementation class
+ */
+public class Book {
+    /*
+     * Address implementation class
+     */
+    public static class Address {
+
+        /* Fields */
+        private final String street; // Street name
+        private final int house; // House number
+        private final int flat; // Flat number
+
+
+        /* Constructors */
+        public Address(String street, int house, int flat) {
+            if (street == null || house <= 0 || flat <= 0) {
+                throw new IllegalArgumentException("Incorrect address format with params: " + street + ", " + house + ", " + flat);
+            }
+            this.street = street;
+            this.house = house;
+            this.flat = flat;
+        }
+
+
+        /* Methods */
+
+        /*
+         * Get address street method
+         * Params: None
+         * Returns: (String) Address street name
+         */
+        public String getStreet() {
+            return this.street;
+        }
+
+        /*
+         * Get address house method
+         * Params: None
+         * Returns: (int) Address house number
+         */
+        public int getHouse() {
+            return this.house;
+        }
+
+        /*
+         * Get address flat method
+         * Params: None
+         * Returns: (int) Address flat number
+         */
+        public int getFlat() {
+            return this.flat;
+        }
+
+
+        /* Default override methods */
+
+        @Override
+        public String toString() {
+            return this.street + ", д. " + this.house + ", кв. " + this.flat;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.street.hashCode() + this.house * 2 + this.flat * 3;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj instanceof Address) {
+                Address other = (Address) obj;
+                return this.street.equals(other.street) &&
+                        this.house == other.house &&
+                        this.flat == other.flat;
+            }
+            return false;
+        }
+    }
+
+    /* Fields */
+    private final Map<String, Address> book; // Basic container
+
+
+    /* Constructors */
+
+    /* Default constructor */
+    public Book() {
+        this.book = new HashMap<>();
+    }
+
+    /* Constructor from Map */
+    public Book(Map<String, Address> b) {
+        this.book = new HashMap<>();
+        this.book.putAll(b);
+    }
+
+
+    /* Methods */
+
+    /* Check if book has specified person
+     * Params: (String) person - key to be checked
+     * Returns: true if this book doesn't have a specified person
+     */
+    private boolean hasNo(String person) {
+        return this.book.isEmpty() || !this.book.containsKey(person);
+    }
+
+    /* Add person to book method
+     * Params:
+     *  - (String) person
+     *      Person (key)
+     *  - (Address) ad
+     *      Person's address (value)
+     * Returns: true if the person was added, false otherwise
+     */
+    public boolean add(String person, Address ad) {
+        if (this.book.containsKey(person))
+            return false;
+        this.book.put(person, ad);
+        return true;
+    }
+
+    /* Delete person from book method
+     * Params: (String) person - key to be deleted
+     * Returns: true if the person was deleted, false otherwise
+     */
+    public boolean delete(String person) {
+        if (this.hasNo(person))
+            return false;
+        this.book.remove(person);
+        return true;
+    }
+
+    /* Replace person's address in the book method
+     * Params:
+     *  - (String) person
+     *      Person (key)
+     *  - (Address) ad
+     *      Person's new address (value)
+     * Returns: true if the person's address was replaced, false otherwise
+     */
+    public boolean replace(String person, Address ad) {
+        if (this.hasNo(person))
+            return false;
+        this.book.replace(person, ad);
+        return true;
+    }
+
+    /* Get address by person method
+     * Params: (String) person - key
+     * Returns: (Address) of the person or null if this book contains no mapping for the key
+     */
+    public Address getAddress(String person) {
+        return this.book.get(person);
+    }
+
+    /* Get set of people living on the specified street
+     * Params: (String) street - the street to be examined
+     * Returns: (Set<String>) Set of people who live on the specified street
+     */
+    public Set<String> getPeople(String street) {
+        Set<String> res = new HashSet<>();
+
+        for (String p : this.book.keySet()) {
+            Address ad = getAddress(p);
+            if (street.equals(ad.getStreet()))
+                res.add(p);
+        }
+
+        return res;
+    }
+
+    /* Get set of people living on the specified street AND in the specified house
+     * Params:
+     *  - (String) street - the street to be examined
+     *  - (int) house - the number of house to be examined
+     * Returns: (Set<String>) List of people who live on the specified street AND in the specified house
+     */
+    public Set<String> getPeople(String street, int house) {
+        Set<String> res = new HashSet<>();
+
+        for (String p : this.book.keySet()) {
+            Address ad = getAddress(p);
+            if (street.equals(ad.getStreet()) && house == ad.getHouse())
+                res.add(p);
+        }
+
+        return res;
+    }
+
+    /* Get book size method
+     * Params: None
+     * Returns: (int) the size of the book
+     */
+    public int size() {
+        return this.book.size();
+    }
+
+
+    /* Default override methods */
+
+    @Override
+    public String toString() {
+        return this.book.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.book.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj instanceof Book) {
+            Book other = (Book) obj;
+            return this.book.equals(other.book);
+        }
+        return false;
+    }
+}
---
+++ src/test/java/coursework_sem2_t1/BookTest.java
@@ -0,0 +1,190 @@
+/* FILE NAME: BookTest.java
+ * PROGRAMMER: Ilyin Vladimir, group 3530901/10005, GitHub: @wishyoudie
+ * DATE: 02/23/2022
+ * PURPOSE: Implementation of Address book class.
+ *          Test module.
+ */
+
+/* Package */
+package coursework_sem2_t1;
+
+/* Imports */
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
+/*
+ * Address book test class
+ */
+class BookTest {
+    @Test
+    public void addressStreet() {
+        String st = "a";
+        Book.Address ad = new Book.Address("a", 1, 1);
+        assertEquals(st, ad.getStreet());
+    }
+
+    @Test
+    public void badAddressStreet() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Book.Address bad = new Book.Address(null, 1, 1);
+        });
+    }
+
+    @Test
+    public void addressHouse() {
+        int h = 1;
+        Book.Address ad = new Book.Address("a", 1, 1);
+        assertEquals(h, ad.getHouse());
+    }
+
+    @Test
+    public void badAddressHouse() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Book.Address bad = new Book.Address("null", -1, 1);
+        });
+    }
+
+    @Test
+    public void addressFlat() {
+        int fl = 2;
+        Book.Address ad = new Book.Address("a", 1, 2);
+        assertEquals(fl, ad.getFlat());
+    }
+
+    @Test
+    public void badAddressFlat() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Book.Address bad = new Book.Address("null", 1, -1);
+        });
+    }
+
+
+    @Test
+    void add() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        assertEquals(0, a.size());
+        assertTrue(a.add("A", ad));
+        assertEquals(1, a.size());
+        assertFalse(a.add("A", ad));
+        Book.Address adt = new Book.Address("a", 1 ,2);
+        assertFalse(a.add("A", adt));
+    }
+
+    @Test
+    void secondConstructor() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        a.add("A", ad);
+        Map<String, Book.Address> bMap = new HashMap<>();
+        bMap.put("A", ad);
+        Book b = new Book(bMap);
+        assertEquals(a, b);
+    }
+
+    @Test
+    void delete() {
+        Book a = new Book();
+        assertFalse(a.delete("B"));
+        Book.Address ad = new Book.Address("a", 1, 1);
+        a.add("A", ad);
+        assertTrue(a.delete("A"));
+        assertEquals(0, a.size());
+        assertFalse(a.delete("B"));
+    }
+
+    @Test
+    void replace() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        assertFalse(a.replace("A", ad));
+        Book.Address ab = new Book.Address("a", 1, 2);
+        a.add("A", ad);
+        assertTrue(a.replace("A", ab));
+        assertEquals(a.getAddress("A"), ab);
+        assertFalse(a.replace("B", ad));
+    }
+
+    @Test
+    void getAddress() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        a.add("A", ad);
+        assertEquals(ad, a.getAddress("A"));
+        assertNull(a.getAddress("B"));
+        a.delete("A");
+        assertNull(a.getAddress("A"));
+    }
+
+    @Test
+    void getPeople() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        a.add("A", ad);
+        a.add("B", ad);
+        a.add("C", ad);
+        Set<String> GoodGuys = new HashSet<>();
+        GoodGuys.add("A");
+        GoodGuys.add("B");
+        GoodGuys.add("C");
+        assertEquals(GoodGuys, a.getPeople("a"));
+        a.delete("C");
+        GoodGuys.remove("C");
+        assertEquals(GoodGuys, a.getPeople("a"));
+        GoodGuys.add("D");
+        assertNotEquals(GoodGuys, a.getPeople("a"));
+        assertEquals(new HashSet<String>(), a.getPeople("b"));
+    }
+
+    @Test
+    void getPeopleSecond() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        a.add("A", ad);
+        Book.Address ab = new Book.Address("a", 1, 2);
+        a.add("B", ab);
+        Book.Address al = new Book.Address("a", 2, 1);
+        a.add("C", al);
+        Set<String> GoodGuys = new HashSet<>();
+        GoodGuys.add("A");
+        GoodGuys.add("B");
+        assertEquals(GoodGuys, a.getPeople("a", 1));
+        Set<String> BadGuys = new HashSet<>();
+        BadGuys.add("C");
+        assertEquals(BadGuys, a.getPeople("a", 2));
+        GoodGuys.addAll(BadGuys);
+        assertEquals(GoodGuys, a.getPeople("a"));
+        assertEquals(new HashSet<>(), a.getPeople("a", 3));
+    }
+
+    @Test
+    void size() {
+        Book a = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        assertEquals(0, a.size());
+        a.add("a", ad);
+        assertEquals(1, a.size());
+        a.add("a", ad);
+        assertEquals(1, a.size());
+        a.delete("a");
+        assertEquals(0, a.size());
+    }
+
+    @Test
+    void testEquals() {
+        Book a = new Book();
+        Book b = new Book();
+        Book.Address ad = new Book.Address("a", 1, 1);
+        Book.Address ab = new Book.Address("a", 1, 2);
+        a.add("a", ad);
+        b.add("a", ad);
+        assertEquals(a, b);
+        b.add("b", ab);
+        assertNotEquals(a, b);
+    }
+}
