597e01abfa386ff8155a19982dd44d2120bcd65a
wishyoudie <66038379+wishyoudie@users.noreply.github.com>
2022-02-23 20:49:29 +0300

    Commit

--- src/main/java/coursework_sem2_t1/Book.java
+++ src/main/java/coursework_sem2_t1/Book.java
@@ -19,6 +19,7 @@ import java.util.HashSet;
  * Address book implementation class
  */
 public class Book {
+
     /*
      * Address implementation class
      */
--- src/test/java/coursework_sem2_t1/BookTest.java
+++ src/test/java/coursework_sem2_t1/BookTest.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
+
 /*
  * Address book test class
  */
